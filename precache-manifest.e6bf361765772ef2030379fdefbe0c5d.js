14c8b0a147b416fb1b142cf776e44ad070cdcd44
Oles Marola <oles_pw@ukr.net>
2020-01-17 16:53:51 +0200

    Updates

---
+++ precache-manifest.e6bf361765772ef2030379fdefbe0c5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3a79a224a784fad760f9b97a3219bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e3a16e19b6030faf45",
+    "url": "/static/css/main.24c6d906.chunk.css"
+  },
+  {
+    "revision": "e381f50f1646370bd2dd",
+    "url": "/static/js/2.1bf34961.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.1bf34961.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26e3a16e19b6030faf45",
+    "url": "/static/js/main.d96e57b8.chunk.js"
+  },
+  {
+    "revision": "5dcce75dc85f0a04ea2f",
+    "url": "/static/js/runtime-main.c734b847.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,3 +0,0 @@
-export const TERM_PREFIX = 's-';
-export const BRAND_TERM_PREFIX = 'b-';
-export const STYLE_PREFIX = 'st-';
